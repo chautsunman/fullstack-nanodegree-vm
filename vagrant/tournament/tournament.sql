f38aa002adab070049965bae85c094e731744fb7
Chau Tsun Man <tsunman.chau96@gmail.com>
2016-05-15 15:12:19 +0800

    Update table Players design

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -11,6 +11,6 @@ DROP DATABASE IF EXISTS tournament;
 CREATE DATABASE tournament;
 \c tournament;
 
-CREATE TABLE Players (id SERIAL PRIMARY KEY, name TEXT);
+CREATE TABLE Players (id SERIAL PRIMARY KEY, name TEXT, wins INTEGER DEFAULT 0, matches INTEGER DEFAULT 0);
 
 CREATE TABLE Matches (id SERIAL PRIMARY KEY, player1 INT REFERENCES Players (id), player2 INT REFERENCES Players (id));
