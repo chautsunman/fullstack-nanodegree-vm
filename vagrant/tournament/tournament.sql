857187e1b20724727a7fa724712319ba4fda9e56
Chau Tsun Man <tsunman.chau96@gmail.com>
2016-05-15 15:23:05 +0800

    Update table Matches design

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -13,4 +13,4 @@ CREATE DATABASE tournament;
 
 CREATE TABLE Players (id SERIAL PRIMARY KEY, name TEXT, wins INTEGER DEFAULT 0, matches INTEGER DEFAULT 0);
 
-CREATE TABLE Matches (id SERIAL PRIMARY KEY, player1 INT REFERENCES Players (id), player2 INT REFERENCES Players (id));
+CREATE TABLE Matches (id SERIAL PRIMARY KEY, winner INT REFERENCES Players (id), loser INT REFERENCES Players (id));
